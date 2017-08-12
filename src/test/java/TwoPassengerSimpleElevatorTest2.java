013a386f1cae8a06a5e5269b4749fbd1a640b101
Zhang <yaguang.zhang@sap.com>
2017-08-12 11:05:44 +0800

    verify the  two person in the elevator

--- src/test/java/TwoPassengerSimpleElevatorTest2.java
+++ src/test/java/TwoPassengerSimpleElevatorTest2.java
@@ -16,15 +16,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @RunWith(Parameterized.class)
 public class TwoPassengerSimpleElevatorTest2 extends  ElevatorBaseTest {
 
-
-    @Parameterized.Parameters
-    public  static Collection<Object[]> getModel(){
-
-        return  Arrays.asList( new Object[][]{
-                {"1. ", new int[]{}, new int[]{}, new int[]{}},
-        });
-    }
-
     private final int[] expected;
     private final String description;
     private final int[] from;
@@ -39,12 +30,42 @@ public class TwoPassengerSimpleElevatorTest2 extends  ElevatorBaseTest {
 
     }
 
+
+    @Parameterized.Parameters
+    public  static Collection<Object[]> getModel(){
+
+        return  Arrays.asList( new Object[][]{
+                {"1. ", new int[]{}, new int[]{}, new int[]{}},
+                {"2. ", new int[]{1,1}, new int[]{1,1}, new int[]{1}}
+                ,
+                {"3. ", new int[]{1,1}, new int[]{2,3}, new int[]{1,2,3}},
+                {"4. ", new int[]{1,1}, new int[]{-2,-3}, new int[]{1,0,-1,-2,-3}},
+                {"5. ", new int[]{1,1}, new int[]{-2,2}, new int[]{1,2,1,0,-1,-2}},
+                {"6. ", new int[]{1,1}, new int[]{2,-2}, new int[]{1,2,1,0,-1,-2}},
+
+                {"7. ", new int[]{2,3}, new int[]{2,3}, new int[]{1,2,3}},
+                {"8. ", new int[]{2,3}, new int[]{3,4}, new int[]{1,2,3,4}},
+                {"9. ", new int[]{2,3}, new int[]{4,5}, new int[]{1,2,3,4,5}},
+                {"10. ", new int[]{2,3}, new int[]{2,0}, new int[]{1,2,3,2,1,0}},
+                {"11. ", new int[]{2,3}, new int[]{-1,-2}, new int[]{1,2,3,2,1,0,-1,-2}},
+
+
+                {"12. ", new int[]{-2,-3}, new int[]{0,1}, new int[]{1,0,-1,-2,-3,-2,-1,0,1}},
+                {"13. ", new int[]{-2,-3}, new int[]{3,4}, new int[]{1,0,-1,-2,-3,-2,-1,0,1,2,3,4}},
+                {"14. ", new int[]{2,3}, new int[]{-4,-5}, new int[]{1,2,3,2,1,0,-1,-2,-3,-4,-5}},
+
+
+        });
+    }
+
     @Test
-    public void test() throws Exception {
+    public void twoPasserTestCase() throws Exception {
 
         elevator.setFloor(1);
         Arrays.stream(this.from).forEach(e->elevator.requestFromFloor(e));
         Arrays.stream(this.to).forEach(e->elevator.requestTo(e));
+
+        elevator.run();
 
         int[] actual = listner.getEventHistory().stream().mapToInt(e -> e.getFloor()).toArray();
         assertThat(description, actual,is(expected));
